a066e707646720c3383b11b0766b9f4cb858eb94
Lt-Henry <quiqueiii@gmail.com>
2022-10-26 16:20:39 +0200

    wip on atlas code, based on previous projects

--- atlas.cpp
+++ atlas.cpp
@@ -4,6 +4,8 @@
 
 #include "atlas.hpp"
 
+#include <SDL2/SDL_image.h>
+
 #include <iostream>
 #include <fstream>
 
@@ -11,6 +13,38 @@ using namespace lluna;
 
 using namespace std;
 
+class StaticLoader
+{
+    StaticLoader()
+    {
+        if (IMG_Init() == 0) {
+            cerr<<"Failed to init SDL_Image"<<endl;
+        }
+    }
+
+    ~StaticLoader()
+    {
+        IMG_Quit();
+    }
+};
+
+StaticLoader loader;
+
 Atlas::Atlas(const char* filename)
 {
+    //SDL_Renderer* renderer = Game::get()->renderer;
+    SDL_Surface* data = IMG_Load(filename);
+
+    if (!data) {
+        cerr<<"Failed to load image:"<<filename<<endl;
+    }
+
+    //this->texture = SDL_CreateTextureFromSurface(renderer,data);
+
+    SDL_FreeSurface(data);
+}
+
+Atlas::~Atlas()
+{
+    SDL_DestroyTexture(texture);
 }
--- atlas.hpp
+++ atlas.hpp
@@ -2,11 +2,15 @@
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+#include <SDL2/SDL.h>
+
 namespace lluna
 {
     class Atlas
     {
         public:
+
+            SDL_Texture* texture;
 
             Atlas(const char* filename);
             ~Atlas();
